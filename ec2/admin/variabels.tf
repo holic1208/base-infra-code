d40549a4a8147f239268f7eb73e7427eccc5cadf
sangun <holic1208@daum.net>
2024-11-13 00:20:21 +0900

    generating ec2 code

---
+++ ec2/admin/variabels.tf
@@ -0,0 +1,15 @@
+variable "account_id" {
+  default = ["985522651362"]
+}
+
+variable "region" {
+  default = "ap-northeast-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "name" {
+  default = "kubectl"
+}
